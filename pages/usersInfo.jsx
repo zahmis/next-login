492aca7d2364cacf272cd928a2a466e0cd2919d5
zahmis <meda829ratteru86@gmail.com>
2021-08-19 00:56:05 +0900

    move container

--- pages/usersInfo.jsx
+++ pages/usersInfo.jsx
@@ -20,17 +20,23 @@ export default function UsersInfo() {
   return (
     <>
       {users == null ? "Now Loading" : users.map((user) =>     
-      <div className="container">
-        <div class="row card">
-          <div class="card-body">
-            <div>
-              <Image src={user.avatar} width="64px" height="64px"/>
-              <a href="#" class="btn btn-primary">Show Detail</a>
-              name {user.first_name}{user.last_name} 
-              email {user.email}
+        <div className="card">
+          <div className="card-body">
+            <div className="container">
+              <div className="row">
+                  <div className="col-1">
+                    <Image src={user.avatar} width="64px" height="64px"/>
+                  </div>
+                  <div className="col-2">
+                    <a href="#" className ="btn btn-primary d-flex justify-content-center a">SHOW DETAIL</a>
+                  </div>
+              </div>
+              <div className="row">
+                <span>name &nbsp;&nbsp;　{user.first_name}{user.last_name}</span>
+                <span>email &nbsp;&nbsp;　{user.email}</span>
+              </div>
             </div>
           </div>
-        </div>
       </div>   
      
         )}
